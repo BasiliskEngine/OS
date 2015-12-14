41fde759be125cc826459d18d345de1c2a6cd852
Garrett Stewart <roodymawnstr@gmail.com>
2015-12-14 12:52:39 -0800

    Created cursor variables

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
+#include "monitor.h"
+
 int main(struct multiboot *mboot_ptr)
 {
+	monitor_clear();
+	monitor_write("Hello, world!");
 	return 0xDEADBABA;
 }
--- src/monitor.h
+++ src/monitor.h
@@ -3,6 +3,10 @@
 
 #include "common.h"
 
+u16int *video_memory = (u16int *) 0xB8000;
+u8int cursor_x = 0;
+u8int cursor_y = 0;
+
 void monitor_put(char c);
 void monitor_clear();
 void monitor_write(char *c);
