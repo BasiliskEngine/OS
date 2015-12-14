49ca4bfe19a0786479e5887f413aa3c27b05c68f
Garrett Stewart <roodymawnstr@gmail.com>
2015-12-14 10:34:53 -0800

    First boot

--- run_bochs.sh
+++ run_bochs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run_bochs.sh
-# mounths the correct loopback device, runs bochs, then unmounts.
+# mounts the correct loopback device, runs bochs, then unmounts.
 
 sudo /sbin/losetup /dev/loop0 floppy.img
 sudo bochs -f bochsrc.txt
